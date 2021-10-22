25bd61623cdf0a23d09f3985338fcde21e201e7a
alvaro-j <alvarojaime360@gmail.com>
2021-10-22 10:01:41 -0300

    remove console.log(data)

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const getGithubData = async () => {
   const userURL = "http://api.github.com/users/alvaro-j";
   const res = await fetch(userURL);
   const data = await res.json();
-  console.log(data);
   githubUserPhoto.src = data.avatar_url;
   githubName.innerHTML = data.name;
   githubUserURL.href = data.html_url;
