decc83c5fd23282a89923d55ca4e754ab8b825c0
alvaro-j <alvarojaime360@gmail.com>
2021-10-22 09:32:30 -0300

    add fetch and api complete

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const getGithubData = async () => {
   const res = await fetch(userURL);
   const data = await res.json();
   console.log(data);
+  githubUserPhoto.src = data.avatar_url;
+  githubName.innerHTML = data.name;
+  githubUserURL.href = data.html_url;
+  githubUserName.innerHTML = data.login;
+  githubBio.innerHTML = data.bio;
 }
 
 getGithubData();
